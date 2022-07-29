bd0796d4f56cd81db8a9cb2053464b9414a6e3d8
Dietmar <tcodmos@gmail.com>
2022-07-29 22:02:03 +0300

    init pro

---
+++ bin/codewars_dart.dart
@@ -0,0 +1,5 @@
+import 'package:codewars_dart/codewars_dart.dart' as codewars_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${codewars_dart.calculate()}!');
+}
---
+++ test/codewars_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:codewars_dart/codewars_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
